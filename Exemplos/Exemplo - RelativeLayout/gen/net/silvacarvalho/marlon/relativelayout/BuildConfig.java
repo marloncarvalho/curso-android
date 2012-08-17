9d497dcb67f76f3e4989e9f9f56c3b8872fe3e54
Marlon Silva Carvalho <marlon.carvalho@gmail.com>
2012-08-17 14:44:01 -0300

    Novo Projeto

---
+++ Exemplos/Exemplo - RelativeLayout/gen/net/silvacarvalho/marlon/relativelayout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.silvacarvalho.marlon.relativelayout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
